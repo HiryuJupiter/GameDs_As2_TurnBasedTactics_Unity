0d3ade139508038604b85343005359a35a6f7fc1
Dreadbread76 <61036310+Dreadbread76@users.noreply.github.com>
2021-04-19 00:19:55 +1000

    hex

--- Assets/Scripts/Units/UnitManager.cs
+++ Assets/Scripts/Units/UnitManager.cs
@@ -9,10 +9,16 @@ namespace Units
     {
         public float laneDistX = 0f;
         public float laneDistZ = 1.04f;
+        public float offsetDist;
         public int horiLanes;
         public int[] vertLanes = new int[2];
         bool spawningBlue = true;
-        
+        bool oddLane;
+        int colour;
+        int currentLaneSpawned;
+        public GameObject[,] hexPoints;
+
+
         public GameObject[] hex = new GameObject[2];
         public Transform spawnStart;
         
@@ -21,20 +27,60 @@ namespace Units
         // Start is called before the first frame update
         void Start()
         {
+            spawnStart.position = new Vector3(0, 0, 0);
             spawningBlue = true;
-            Transform[,] hexPoints = new Transform[vertLanes[0] + vertLanes[1], horiLanes];
-            SpawnColour(0);
+            oddLane = true;
+            currentLaneSpawned = 0;
+            hexPoints = new GameObject[vertLanes[0] + vertLanes[1], horiLanes];
+            SpawnColour();
         }
       
-        public void SpawnColour(int colour)
+        public void SpawnColour()
         {
+            
+            if(spawningBlue == true)
+            {
+                colour = 0;
+            }
+            else
+            {
+                colour = 1;
+            }
             for (int i = 0; i < vertLanes[colour]; i++)
             {
+
                 GameObject currentHex = Instantiate(hex[colour], spawnStart.position, spawnStart.rotation);
 
+                hexPoints[i + colour*vertLanes[colour], currentLaneSpawned] = currentHex;
+
                 spawnStart.position = new Vector3(spawnStart.position.x, spawnStart.position.y, spawnStart.position.z + laneDistZ);
-               
+
             }
+
+            if (!spawningBlue)
+            {
+                currentLaneSpawned++;
+                if (oddLane)
+                {
+                    spawnStart.position = new Vector3(0.9f * currentLaneSpawned, 0, -0.52f);
+                }
+                else
+                {
+                    spawnStart.position = new Vector3(0.9f * currentLaneSpawned, 0, 0);
+                }
+                // Change from odd lane to even
+                oddLane = !oddLane;
+            }
+           
+            // Change hexagon colour
+            spawningBlue = !spawningBlue;
+
+            
+            if (currentLaneSpawned < horiLanes)
+            {
+                SpawnColour();
+            }
+            
 
         }
         public void EndSpawning()
