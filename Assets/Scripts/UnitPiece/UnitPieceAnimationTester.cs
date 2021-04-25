13d0c5bf99357331367cd561d95f3cdd9ce77070
HiryuJupiter <xooase@gmail.com>
2021-04-25 16:18:19 +1000

    Imported 3 models

---
+++ Assets/Scripts/UnitPiece/UnitPieceAnimationTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UnitPieceAnimationTester : MonoBehaviour
+{
+    UnitPiece unit;
+
+    void Start()
+    {
+        unit = GetComponent<UnitPiece>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            unit.PlayIdle();
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            unit.PlayDead();
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            unit.PlayMoving();
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            unit.PlayAttack();
+    }
+}
